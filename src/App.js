046224c2ec82af6d2725c6a90bee675111e49c17
Tessa <tessa.seiders@gmail.com>
2020-06-26 23:28:30 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="fullName">Haley Seiders</p>
+        <image src=""></image>
+        <button>Really big button</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,
+  document.getElementById('root')
+);
